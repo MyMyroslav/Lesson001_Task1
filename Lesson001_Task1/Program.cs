54d270b70b7c2f50d98802f0f0235b6878eb7e83
Myroslav <havrushkomyroslav@gmail.com>
2020-10-22 20:13:56 +0300

    Done-Lesson001_Task1

---
+++ Lesson001_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson001_Task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            sbyte a = 2;
+            byte b = 16;
+            short c = 5;
+            ushort d = 12;
+            int e = -5;
+            uint f = 3;
+            long g = 1123123123;
+            ulong h = 98989898;
+            Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h);
+        }
+    }
+}
